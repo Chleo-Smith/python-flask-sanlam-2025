9d0385eb384b89bcf296374654c7139958455db1
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-03-28 10:05:24 +0200

    add seed stuff

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
+from os import environ
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config:
-    # General pattern
-    # mssql+pyodbc://<username>:<password>@<dsn_name>?driver=<driver_name>
-    # mssql+pyodbc://@<server_name>/<db_name>?driver=<driver_name>
-    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://@PF1BTSVQ\SQLEXPRESS/SanlamMovieDB2025?driver=ODBC+Driver+17+for+SQL+Server"
+    #     #     # General pattern
+    #     #     # mssql+pyodbc://<username>:<password>@<dsn_name>?driver=<driver_name>
+    #     #     # mssql+pyodbc://@<server_name>/<db_name>?driver=<driver_name>
+    SQLALCHEMY_DATABASE_URI = environ.get("LOCAL_DATABASE_URL")
     SQLALCHEMY_MODIFICATION_TRACK = False
