d9d48ca383ee3bba15180732ae0cc634c17040ef
Chleo-Smith <Chleo.Smith@sanlam.co.za>
2025-03-31 10:23:21 +0200

    coditional rendering - if

--- routes/main_bp.py
+++ routes/main_bp.py
@@ -19,16 +19,19 @@ hobbies = [
 
 users = [
     {
-        "name": "Cleo 🐍💛",
+        "name": "Cleo",
         "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1fpi9gj1LTsGpasZaH4-zMVUIeJTTa7tBqg&s",
+        "isVerified": False,
     },
     {
-        "name": "Draculaura 💗🦇",
+        "name": "Draculaura",
         "image": "https://qudahalloween.com/cdn/shop/articles/Draculaura-Cosplay-featured_600x.jpg?v=1719395794",
+        "isVerified": True,
     },
     {
-        "name": "Frankie ⚡🔩",
+        "name": "Frankie",
         "image": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ad9e988f-fc92-4b5e-83ca-bcffbb33f48e/ddus7aw-a876f565-c1e8-44d3-b1db-6cae6713392f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FkOWU5ODhmLWZjOTItNGI1ZS04M2NhLWJjZmZiYjMzZjQ4ZVwvZGR1czdhdy1hODc2ZjU2NS1jMWU4LTQ0ZDMtYjFkYi02Y2FlNjcxMzM5MmYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.K8HdZZznedNY8zU4iWytUBYrmxSzrg0QI4i3gy0RWR4",
+        "isVerified": False,
     },
 ]
 
--- routes/movies_list_bp.py
+++ routes/movies_list_bp.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 
-from flask import Blueprint, Flask, json, render_template, request
+from flask import Blueprint, Flask, json, redirect, render_template, request, url_for
 
 from constants import STATUS_CODE
 from extensions import db
@@ -33,3 +33,49 @@ def movie_details_page(id):
 
     data = movie.to_dict()
     return render_template("movie-details.html", movie=data)
+
+
+@movies_list_bp.get("/new")
+def add_movie_page():
+    return render_template("add-movie.html")
+
+
+@movies_list_bp.post("/")
+def create_movie():
+    data = {
+        "name": request.form.get("name"),
+        "poster": request.form.get("poster"),
+        "rating": request.form.get("rating"),
+        "summary": request.form.get("summary"),
+        "trailer": request.form.get("trailer"),
+    }
+    # data = request.get_json()  # body
+    new_movie = Movie(**data)
+
+    try:
+        # print(new_movie, new_movie.to_dict())
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("movies_list_bp.movies_list_page"))
+    except Exception as e:
+        db.session.rollback()  # Undo: Restore the data | After commit cannot undo
+        return redirect(url_for("movies_list_bp.add_movie_page"))
+
+
+# DELETE ON BUTTON CLICK
+@movies_list_bp.post("/<id>")
+def delete_movie_page_by_id(id):
+    movie = Movie.query.get(id)
+
+    if not movie:
+        return render_template("not-found.html"), STATUS_CODE["NOT FOUND"]
+
+    try:
+        data = movie.to_dict()
+        db.session.delete(movie)  # errors can happen here
+        db.session.commit()  # anything that we change needs to be committed (del/update/create)
+        return redirect(url_for("movies_list_bp.movies_list_page"))
+    except Exception as e:
+        db.session.rollback()  # undo: restore the data but after commit cannot undo
+        # TO DO
+        return redirect(url_for("movies_list_bp.movies_list_page"))
